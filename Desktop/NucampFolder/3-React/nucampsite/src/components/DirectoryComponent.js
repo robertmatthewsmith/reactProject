7f1ec093036993543d94efd93856bc88977417ab
Robert Smith <robertmatthewsmith1976@gmail.com>
2020-10-22 19:22:48 -0500

    10/22(2)

--- Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return (
-        <Card onClick ={() => onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt = {campsite} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+        <Card>
+            <CardImg width="100%" src={campsite.image} alt = {campsite} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
     );
 }
 
@@ -18,7 +18,7 @@ function Directory(props) {
         const directory = props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                    <RenderDirectoryItem campsite={campsite} />
                 </div>
             );
         });
--- Desktop/NucampFolder/3-React/nucampsite/src/components/MainComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/MainComponent.js
@@ -2,7 +2,9 @@ import React, { Component} from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent'; 
 import Header from './HeaderComponent';
-import Footer from './FooterComponent'
+import Footer from './FooterComponent';
+import Home from './HomeComponent'
+import { Switch, Route, Redirect } from 'react-router-dom';
 import {CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
@@ -10,20 +12,24 @@ class Main extends Component {
         super(props);
         this.state= { 
             campsites: CAMPSITES,
-            selectedCampSites: null
         };
     }
 
-    onCampSiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
   render() {
+
+    const HomePage =() => {
+        return (
+            <Home />
+        );
+    }
       return (
           <div>
               <Header />
-              <Directory campsites ={this.state.campsites} onClick={campsiteId => this.onCampSiteSelect(campsiteId)}/>
-              <CampsiteInfo campsite = {this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+              <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} /> } />
+                    <Redirect to='/home' />
+              </Switch>
               <Footer />
           </div>
       );

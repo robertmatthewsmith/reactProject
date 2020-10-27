060bbc2d77e8c3d73457be2367923d62237c7abe
Robert Smith <robertmatthewsmith1976@gmail.com>
2020-10-27 12:17:29 -0500

    10/27 12pm

--- Desktop/NucampFolder/3-React/nucampsite/src/components/MainComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/MainComponent.js
@@ -47,7 +47,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus'render={() => <About partners={this.state.partners}/>}/>
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners}/>}/>
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />

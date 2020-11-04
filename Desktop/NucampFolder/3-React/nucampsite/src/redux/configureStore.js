e9de168c8ab6691d7071dfba6b44d89fb5a02b55
Robert Smith <robertmatthewsmith1976@gmail.com>
2020-11-04 10:16:41 -0600

    React-Redux-Form Revisited

--- Desktop/NucampFolder/3-React/nucampsite/src/redux/configureStore.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/redux/configureStore.js
@@ -1,20 +1,23 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-
+import { createForms } from 'react-redux-form';
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-export const ConfigureStore = () => {
+import { InitialFeedback } from './forms';
 
+export const ConfigureStore = () => {
     const store = createStore(
-        
         combineReducers({
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
